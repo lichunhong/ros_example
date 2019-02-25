653371c2f86011fe1652ef57a4f53db25960dfcf
lichunhong <1028960614@qq.com>
2019-02-25 20:32:30 +0800

    display urdf in rviz

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = "/home/lichunhong/Documents/src/effective_robotics_programming_with_ros-master/catkin_ws/src"
 whitelisted_packages = "".split(';') if "" != "" else []
 blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/lichunhong/Documents/src/effective_robotics_programming_with_ros-master/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/lichunhong/Documents/src/effective_robotics_programming_with_ros-master/catkin_ws/devel;/opt/ros/kinetic" != "" else []
+underlay_workspaces = "/opt/ros/kinetic".split(';') if "/opt/ros/kinetic" != "" else []
--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,6 +7,11 @@
 # new environment variables
 
 # modified environment variables
-export LD_LIBRARY_PATH="/home/lichunhong/Documents/src/effective_robotics_programming_with_ros-master/catkin_ws/devel/lib:/opt/ros/kinetic/lib:/opt/ros/kinetic/lib/x86_64-linux-gnu"
-export PKG_CONFIG_PATH="/home/lichunhong/Documents/src/effective_robotics_programming_with_ros-master/catkin_ws/devel/lib/pkgconfig:/opt/ros/kinetic/lib/pkgconfig:/opt/ros/kinetic/lib/x86_64-linux-gnu/pkgconfig"
+export CMAKE_PREFIX_PATH="/home/lichunhong/Documents/src/effective_robotics_programming_with_ros-master/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/lichunhong/Documents/src/effective_robotics_programming_with_ros-master/catkin_ws/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/lichunhong/Documents/src/effective_robotics_programming_with_ros-master/catkin_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/lichunhong/Documents/src/effective_robotics_programming_with_ros-master/catkin_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/lichunhong/Documents/src/effective_robotics_programming_with_ros-master/catkin_ws/devel/share/common-lisp"
+export ROS_MAVEN_DEPLOYMENT_REPOSITORY="/home/lichunhong/Documents/src/effective_robotics_programming_with_ros-master/catkin_ws/devel/share/maven"
+export ROS_MAVEN_PATH="/home/lichunhong/Documents/src/effective_robotics_programming_with_ros-master/catkin_ws/devel/share/maven:$ROS_MAVEN_PATH"
+export ROS_PACKAGE_PATH="/home/lichunhong/Documents/src/effective_robotics_programming_with_ros-master/catkin_ws/src:$ROS_PACKAGE_PATH"
